53d19b8a2f4779bad2bfbb6d9c6c2e2e8c07a92d
MohamedFathi96 <mohamedfathi4345@gmail.com>
2023-04-03 18:01:20 +0200

    navigation

--- src/js/main.js
+++ src/js/main.js
@@ -2,31 +2,40 @@ const slideDownElems = document.querySelectorAll(".down");
 const slideRightElems = document.querySelectorAll(".left");
 const slideLeftElems = document.querySelectorAll(".right");
 
-const slideUpObserver = new IntersectionObserver((entries) => {
-  entries.forEach((elem) => {
-    if (elem.isIntersecting) {
-      elem.target.classList.add("slideUp");
-      slideUpObserver.unobserve(elem.target);
-    }
-  });
-});
+const slideUpObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((elem) => {
+      if (elem.isIntersecting) {
+        elem.target.classList.add("slideUp");
+        slideUpObserver.unobserve(elem.target);
+      }
+    });
+  },
+  { threshold: 0.4 }
+);
 
-const slideRightObserver = new IntersectionObserver((entries) => {
-  entries.forEach((elem) => {
-    if (elem.isIntersecting) {
-      elem.target.classList.add("slideRight");
-      slideRightObserver.unobserve(elem.target);
-    }
-  });
-});
-const slideLeftObserver = new IntersectionObserver((entries) => {
-  entries.forEach((elem) => {
-    if (elem.isIntersecting) {
-      elem.target.classList.add("slideLeft");
-      slideLeftObserver.unobserve(elem.target);
-    }
-  });
-});
+const slideRightObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((elem) => {
+      if (elem.isIntersecting) {
+        elem.target.classList.add("slideRight");
+        slideRightObserver.unobserve(elem.target);
+      }
+    });
+  },
+  { threshold: 0.4 }
+);
+const slideLeftObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((elem) => {
+      if (elem.isIntersecting) {
+        elem.target.classList.add("slideLeft");
+        slideLeftObserver.unobserve(elem.target);
+      }
+    });
+  },
+  { threshold: 0.4 }
+);
 
 slideDownElems.forEach((elem) => slideUpObserver.observe(elem));
 slideRightElems.forEach((elem) => slideRightObserver.observe(elem));
